3f620009ed21951fc45bdd01f04f18f5118319f9
Luis Espinoza <luis.espinoza.nav@live.com>
2024-07-14 22:05:50 -0300

    add redux and root store

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import RootNavigation from "./app/navigation/root-navigation";
+import { Provider } from "react-redux";
+import rootStore from "./app/model/state/root-store";
 
 function App(): React.JSX.Element {
 
 	return (
 		<SafeAreaProvider>
-			<RootNavigation />
+			<Provider store={rootStore}>
+				<RootNavigation />
+			</Provider>
 		</SafeAreaProvider>
 	);
 }
--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from "react";
 import type { PropsWithChildren } from "react";
 import {
@@ -16,77 +9,33 @@ import {
 	useColorScheme,
 	View,
 } from "react-native";
+import { getFullName } from "./../model/state/user/user-slice" 
 
-import {
-	Colors,
-	DebugInstructions,
-	LearnMoreLinks,
-	ReloadInstructions,
-} from "react-native/Libraries/NewAppScreen";
 import { ScreenNames } from "../navigation/screen-names";
 import { navigate } from "../navigation/root-navigation";
-
-type SectionProps = PropsWithChildren<{
-	title: string;
-}>;
-
-function Section({ children, title }: SectionProps): React.JSX.Element {
-	const isDarkMode = useColorScheme() === "dark";
-	return (
-		<View style={styles.sectionContainer}>
-			<Text
-				style={[
-					styles.sectionTitle,
-					{
-						color: isDarkMode ? Colors.white : Colors.black,
-					},
-				]}>
-				{title}
-			</Text>
-			<Text
-				style={[
-					styles.sectionDescription,
-					{
-						color: isDarkMode ? Colors.light : Colors.dark,
-					},
-				]}>
-				{children}
-			</Text>
-		</View>
-	);
-}
+import { useSelector } from "react-redux";
 
 export const Home = ({ route, navigation }): React.JSX.Element => {
+
+	const currentUserFullName = useSelector(getFullName)
 	const isDarkMode = useColorScheme() === 'dark';
 
 	const { name, ocupation } = route?.params;
-
-	const backgroundStyle = {
-		backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-	};
 
 	const goToDetail = () => {
 		navigate(ScreenNames.DETAIL);
 	};
 
 	return (
-		<SafeAreaView style={backgroundStyle}>
-			<ScrollView
-				contentInsetAdjustmentBehavior="automatic"
-				style={backgroundStyle}>
-				<View
-					style={{
-						backgroundColor: isDarkMode
-							? Colors.black
-							: Colors.white,
-					}}>
+	
+			<ScrollView contentInsetAdjustmentBehavior="automatic">
+				<View>
 					<Text>
-						{name} {ocupation}
+						Bienvenido otra vez: {currentUserFullName}
 					</Text>
 					<Button onPress={goToDetail} title="Go to Detail" />
 				</View>
 			</ScrollView>
-		</SafeAreaView>
 	);
 }
 
