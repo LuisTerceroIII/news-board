29cee4acc6ccd4467ff6948e07eac8fe4a226969
Luis Espinoza <luis.espinoza.nav@live.com>
2024-07-15 02:17:20 -0300

    add screen navigation props

--- app/navigation/routes.ts
+++ app/navigation/routes.ts
@@ -1,7 +1,13 @@
 import ArticlesSearchResult from "../screens/articles-search-result";
 import Home from "../screens/home";
 import { ScreenNames } from "./screen-names";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack"
+import { ParamListBase } from "@react-navigation/native"
 
+export interface ScreenNavigationProps {
+	route: { params?: object }
+	navigation: NativeStackNavigationProp<ParamListBase>
+}
 export interface Route {
 	name: string;
 	component: React.FC<{ route: any; navigation: any }>;
